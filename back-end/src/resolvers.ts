4b03c6626b61227ede779ed406dcbddfd4394372
nenamagdalenaa <nena.github@outlook.com>
2025-06-02 09:57:17 +0200

    small fix

--- back-end/src/resolvers.ts
+++ back-end/src/resolvers.ts
@@ -369,7 +369,7 @@ export const resolvers = {
       FROM document d
       WHERE d.embedding <=> $1::vector < 0.2
     )
-    SELECT DISTINCT md.document_id, md.title, md.summary, md.sourceurl, md.dossier_id, md.distance
+    SELECT DISTINCT md.document_id, md.title, md.summary, md.sourceurl, md.dossier_id, md.distance, md.date_scraped
     FROM matched_docs md
     ${joins.replace(/d\./g, 'md.')}
     ${filterSQL.replace(/d\./g, 'md.')}
